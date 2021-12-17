99a3979933cd0afe78ec093235f38f97c6396251
yongchulbag <yongchulbag@gmail.com>
2021-12-18 00:02:45 +0900

    first

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,10 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: <Widget>[
+        Image(image: AssetImage('images/dice1.png'))
+      ]
+    );
   }
 }
